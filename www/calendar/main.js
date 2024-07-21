1cda3d9d1b5a9785a27dbf9e55eacd781b5db418
aww <artur.wyroslak@gmail.com>
2024-07-21 12:39:48 +0200

    test

---
+++ www/calendar/main.js
@@ -0,0 +1,34 @@
+// Load #1, load as little as possible because we are in a race to get the loading screen up.
+define([
+    '/bower_components/nthen/index.js',
+    '/api/config',
+    '/common/dom-ready.js',
+    '/common/sframe-common-outer.js',
+], function (nThen, ApiConfig, DomReady, SFCommonO) {
+
+    var hash, href;
+    nThen(function (waitFor) {
+        DomReady.onReady(waitFor());
+    }).nThen(function (waitFor) {
+        var obj = SFCommonO.initIframe(waitFor, true);
+        href = obj.href;
+        hash = obj.hash;
+    }).nThen(function (/*waitFor*/) {
+        var addData = function (meta, Cryptpad, user, Utils) {
+            if (hash) {
+                var parsed = Utils.Hash.parsePadUrl(href);
+                if (parsed.hashData && parsed.hashData.newPadOpts) {
+                    meta.calendarOpts  = Utils.Hash.decodeDataOptions(parsed.hashData.newPadOpts);
+                }
+            }
+            meta.calendarHash = hash;
+        };
+        SFCommonO.start({
+            addData: addData,
+            hash: hash,
+            href: href,
+            noRealtime: true,
+            cache: true,
+        });
+    });
+});
